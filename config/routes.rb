2defb065a7de467756d74410a3a85132f6f81a46
Carl Manalaysay <carlmanalaysay@Carls-MacBook-Air.local>
2023-03-14 17:53:16 +0800

    added styles and added patch method on update

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get 'articles/new' => 'articles#new', as: 'new_article'
   get 'articles/:id/edit' => 'articles#edit', as: 'edit_article'
   post 'articles/' => 'articles#create', as: 'create_article'
-  post 'articles/:id' => 'articles#update', as: 'update_article'
+  patch 'articles/:id' => 'articles#update', as: 'update_article'
   delete 'articles/:id(.:format)' => 'articles#delete', as: 'delete_article'
 end
